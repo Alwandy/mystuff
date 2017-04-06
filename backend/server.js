efe1fbd17390cd77cbc3638ffb481877c8739ba0
frsotebite <jas.f.ukcmti@gmail.com>
2017-04-06 21:40:35 +0100

    Tweaks to server.js files

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+
+// Get our API routes
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
