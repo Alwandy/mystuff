e5447ea82ee1036bc02c980bc97cb08cab0873f7
frsotebite <jas.f.ukcmti@gmail.com>
2017-04-06 21:18:12 +0100

    Express + New Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,39 @@
+// BASE SETUP
+// =============================================================================
 
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
 
-// Get our API routes
-const api = require('./server/routes/api');
+var mongoose   = require('mongoose');
+mongoose.connect(''); // connect to our database
 
-const app = express();
 
-// Parsers for POST data
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+var port = process.env.PORT || 8080;        // set our port
 
-// Set our api routes
-app.use('/api', api);
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+// more routes for our API will happen here
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
