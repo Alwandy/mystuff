37f8fcf4568659abf67220368b09870fa80271b9
frsotebite <jas.f.ukcmti@gmail.com>
2017-04-07 00:35:53 +0100

    Final cleanup to api framework

--- backend/core/apiprocessor.js
+++ backend/core/apiprocessor.js
@@ -2,27 +2,29 @@ var fs = require('fs');
 var path = require('path');
 var express  = require('express'); 
 const router = express.Router();
-const controllerSuffix = "Controller.js";
+const controllerSuffix = "-controller.js";
 
-var applyAPIDirectory = function(app){
-	fs.readdir("api/", function(err,list){
-		if (err) {
-			throw err;
-		}
-		else if (list == null){
-			return;
-		}
-		for(var i=0; i<list.length; i++)
-		{
-			console.log(list[i]); 
-			if(list[i].includes(controllerSuffix)){
-				var requireName = "../api/" + list[i].replace(".js", "");
-				var apiName = "/"+list[i].replace(controllerSuffix, "");
-				require(requireName)(apiName, router);
-			}
+var applyAPIDirectory = function(app, prefix, rootDir){
+	var files = fs.readdirSync(rootDir);
+	
+	if (files == null){
+		return;
+	}
+	
+	console.log("");
+	console.log("Initialising API directory '" + prefix + "' ...");
+	for(var i=0; i<files.length; i++)
+	{
+		console.log(files[i]); 
+		if(files[i].includes(controllerSuffix)){
+			var requireName = "../" + rootDir + files[i].replace(".js", "");
+			var apiName = "/"+files[i].replace(controllerSuffix, "");
+			require(requireName)(apiName, router);
 			
-			app.use("/api", router);
+			console.log(" - "+prefix+apiName);
 		}
-	});
+		
+		app.use(prefix, router);
+	}
 }
 module.exports.applyAPIDirectory = applyAPIDirectory;
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var apiProcessor = require("./core/apiprocessor");
 
-apiProcessor.applyAPIDirectory(app);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -19,22 +18,10 @@ var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-
-// Get our API routes
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+apiProcessor.applyAPIDirectory(app, "/api", "api/");
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('');
+console.log('Server ready and listening on port ' + port);
