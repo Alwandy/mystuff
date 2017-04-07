ae817199e3a10d6c25f7ac39192ffa0eb3ccfadd
frsotebite <jas.f.ukcmti@gmail.com>
2017-04-07 11:15:35 +0100

    added users controller

---
+++ backend/api/users-controller.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+module.exports =  function(controllerName, router){
+	// Get all posts
+	router.get('/users', (req, res) => {
+	  res.header('Access-Control-Allow-Origin', '*');
+	  MongoClient.connect('mongodb://api:3qBdwMFF8t2m2V5g@92.222.75.92/UKSF', function (err, db) {
+	  if (err) throw err
+
+	  db.collection('Users').find().toArray(function (err, result) {
+		if (err) throw err
+		res.json(result);
+	  })
+	});
+	})
+	return router;
+};
