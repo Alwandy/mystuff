0fa59faf81da89e6a8871432ecc2d0d4ef4df098
Alwandy <alwandy@live.se>
2017-04-10 12:53:50 +0100

    My project added

--- backend/api/users-controller.js
+++ backend/api/users-controller.js
@@ -1,10 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
+var DB = require('../config.js');
 
 module.exports =  function(controllerName, router){
 	// Get all posts
 	router.get(controllerName, (req, res) => {
 	  res.header('Access-Control-Allow-Origin', '*');
-	  MongoClient.connect('mongodb://api:3qBdwMFF8t2m2V5g@92.222.75.92/UKSF', function (err, db) {
+	  MongoClient.connect(DB.dbConfig, function (err, db) {
 	  if (err) throw err
 
 	  db.collection('Users').find().toArray(function (err, result) {
